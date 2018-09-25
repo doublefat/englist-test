be8ae12be982a911c06bc3943447a3d5b59b7840
minfang tao <minfang.tao@oath.com>
2018-09-25 15:43:18 -0700

    add errorlog example

--- app/controllers/example.php
+++ app/controllers/example.php
@@ -1,10 +1,11 @@
 <?php
 
-class ExampleController extends BasicController{
+class ExampleController extends BasicController
+{
 
 
-
-    public function pre_filter(&$methodName = null) {
+    public function pre_filter(&$methodName = null)
+    {
         parent::pre_filter($methodName);
 
         $this->view->addInternalJs("jquery-1.7.1.min.js");
@@ -12,26 +13,27 @@ class ExampleController extends BasicController{
         $this->view->addInternalCss("ui-lightness/jquery-ui-1.8.17.custom.css");
 
 
+    }
 
+    public function index()
+    {
+
+
+        $this->set('exa1', 'hello');
+        $this->set('final_score', 500);
+        $this->view->exa2 = array('world', ' .');
+        error_log("my error log:"."string concat by .");
 
     }
 
-	public function index(){
+    public function ajax()
+    {
+        $this->setLayout("ajax.phtml");
+        $this->set("exa3", "Hello World, ajax");
+    }
 
-
-		$this->set('exa1','hello');
-		$this->set('final_score',500);
-        $this->view->exa2=array('world',' .');
-
-
-	}
-
-	public function ajax(){
-		$this->setLayout("ajax.phtml");
-		$this->set("exa3", "Hello World, ajax");
-	}
-
-	public function phpinfo(){
+    public function phpinfo()
+    {
 
     }
 }
