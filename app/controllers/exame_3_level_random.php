b294b100513b95e92d889dc8f1f65d4461db4b13
chikao0602 <chkao@hotmail.com>
2018-10-05 11:58:40 -0700

    update

--- app/controllers/exame_3_level_random.php
+++ app/controllers/exame_3_level_random.php
@@ -32,13 +32,21 @@ class exame_3_level_randomController extends BasicController
 	echo "easy total = ";
 	dumpHtmlReadable($total);
 	echo "<hr/>";
-	$qs = $qo->getByLevelWithoutUsedIds ( $dbh, 3, 1, array(0) );	// 3 questions, level 1 (easy)
-	$data = $qo->loadFullQuestionsWithOptions ( $dbh, $qs );
-	foreach ( $data as $id=>&$qua ) {
+	// Request for questions and the coresponding options
+	$qs_1 = $qo->getByLevelWithoutUsedIds ( $dbh, 3, 1, array(0) );	// 3 questions, level 1 (easy)
+        $qs_2 = $qo->getByLevelWithoutUsedIds ( $dbh, 3, 2, array(0) );	// 3 questions, level 2 (medium)
+        $qs_3 = $qo->getByLevelWithoutUsedIds ( $dbh, 3, 3, array(0) );	// 3 questions, level 1 (hard)
+	$data_1 = $qo->loadFullQuestionsWithOptions ( $dbh, $qs_1 );
+        $data_2 = $qo->loadFullQuestionsWithOptions ( $dbh, $qs_2 );
+        $data_3 = $qo->loadFullQuestionsWithOptions ( $dbh, $qs_3 );
+        // Put three array into one array
+        $data = array_merge($data_1, $data_2);
+        $data = array_merge($data, $data_3);
+	foreach ( $data as $id=>&$qua ) {		// Pack all the question and options
 	    $this->prepareQuestion ( $qua );
 	}
-	dumpHtmlReadable ( $data );
-	$this->set ( 'data', $data );	// send data to html
+	dumpHtmlReadable ( $data );			// Testing....
+	$this->set ( 'data', $data );			// Send data to html
 	echo '<hr/>';
 
 	// load medium
@@ -60,7 +68,7 @@ class exame_3_level_randomController extends BasicController
 	echo "<hr/>";
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//	$this->view->setTemplate("test/blank.phtml");
+//	$this->view->setTemplate("test/blank.phtml");	// for testing
 
         // Put three array into one array
 
@@ -101,20 +109,73 @@ class exame_3_level_randomController extends BasicController
     }    
 
     public function submit (){
+        echo "POST";
+        dumpHtmlReadable($_POST);
 
-        
        
         $question=array();
         foreach ($_POST as $key=>$value){
              if(strpos($key,"question_")===0 && is_array($value) && !empty($value)){
                  // this is one question answer!!!
-                 echo "find a question:${key} <br/>";
+                 echo "=====find a question:${key} <br/>";
                  $question[]=array("id"=>substr($key,9),'user_answer'=>$value);
              }
         }
-        dumpHtmlReadable($question);
+ 
+        $exam_result = array (0, 0, 0);
         
+        $dbh = connectPDO();
+        $qo = new Questions();
+        $data = $qo->loadFullQuestionsWithOptions ( $dbh, $question );
+       
+        dumpHtmlReadable("data");
+        dumpHtmlReadable($data);
         //$this->redirect("/exame_3_level_random/finish_exame");
+        
+        foreach ( $data as &$user_answer ) {
+            echo 'check an answered question';
+            $q_opt_check = 0;  // 0 : unchecked/right, 1 : wrong
+            foreach ( $user_answer ["options"] as &$db_opt ) {
+                echo 'db_opt';
+                dumpHtmlReadable($db_opt);
+                echo 'db_opt_id = ';
+                dumpHtmlReadable($db_opt ["id"] );
+                $in_usr_opt = 0;
+                foreach ( $user_answer ["question"]["user_answer"] as &$usr_opt ) {
+                    echo 'usr_opts';
+                    dumpHtmlReadable($usr_opt);
+                    if ( $usr_opt == $db_opt ["id"] && $db_opt ["is_right"] == 0 ) {
+                        $q_opt_check = 1;
+                        dumpHtmlReadable ("Answer wrong 0");
+                        dumpHtmlReadable ( $usr_opt );
+                        dumpHtmlReadable ( $$db_opt ["is_right"] );
+                        break;
+                    }
+                    if ( $usr_opt == $db_opt ["id"] ) {
+                       dumpHtmlReadable ( "q_opt_check set!!" ); 
+                       $in_usr_opt = 1;
+                    }
+                }
+                if ( $in_usr_opt == 0 && $db_opt ["is_right"] == 1 ) {
+                    $q_opt_check = 1;
+                    dumpHtmlReadable ( "db_opt [id] : " );
+                    dumpHtmlReadable ( $db_opt [id] );
+                    dumpHtmlReadable ( "in_usr_opt : " );
+                    dumpHtmlReadable ( $in_usr_opt );
+                    dumpHtmlReadable ("Answer Wrong 1");
+                }
+                if ( $q_opt_check == 1 ) {
+                    dumpHtmlReadable ("Answer wrong break!!'");
+                    break;
+                }
+            }
+            if ( $q_opt_check == 0 )
+                echo 'user answer right';
+            else
+                echo 'user answer wrong\n';
+            
+        }
+        
     }
     
     public function finish_exame(){
