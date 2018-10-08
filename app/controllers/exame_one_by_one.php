f9fac61069dff7aea727f16a26118504d709ddb2
chikao0602 <chkao@hotmail.com>
2018-10-08 15:05:19 -0700

    update

--- app/controllers/exame_one_by_one.php
+++ app/controllers/exame_one_by_one.php
@@ -32,14 +32,14 @@ class exame_one_by_oneController extends BasicController
         $qo = new Questions();
         $total = $qo->countByLevel($dbh, 1);
 
-        // Request for an easy question and the coresponding options
+        // Request for a question and the coresponding options
         $qs_1 = $qo->getByLevelWithoutUsedIds($dbh, 1, 1, array(0));    // 1 questions, level 1 (easy)
         $data = $qo->loadFullQuestionsWithOptions($dbh, $qs_1);
         $sel_question =array_values ($data) [0];
 
         $this->prepareQuestion( $sel_question );
-        
         dumpHtmlReadable($sel_question);            // Testing....
+        
         $this->set('question', $sel_question);            // Send data to html
         echo '<hr/>';
     }
@@ -68,13 +68,14 @@ class exame_one_by_oneController extends BasicController
         }
     }
 
-    var $exame_time_second=1*60; // 30 minutes;
+    var $exame_time_second=60*60; // 30 minutes;
 
     public function index()
     {
         if (empty($_SESSION["student"])) {
             $_SESSION["student"] = array("id" => 5, "question_counter" => 0,"start_time"=>time());
-
+            $_SESSION["student"]["score"] = 0;
+            $_SESSION["student"]["question_counter"] = 0;
         }
 
         $now=time();
@@ -113,22 +114,22 @@ class exame_one_by_oneController extends BasicController
     public function finish()
     {
         dumpHtmlReadable($_SESSION["student"]["answers"]);
-
-
+        echo "Score = ";
+        dumpHtmlReadable($_SESSION["student"]["score"]);
     }
 
-    public function submit()
-    {
-
+    public function submit () {
         MLog::iExport($_POST);
         MLog::i("Question id:" . $_POST['question_id']);
 
-        if(empty($_SESSION["student"]["answers"])){
+        if(empty($_SESSION["student"]["answers"])) {
             $_SESSION["student"]["answers"]=array();
         }
-
+        // Get the POST values from sub_bt(Submit my answer) button
         $_SESSION["student"]["answers"][$_POST['question_id']]=array("demo"=>$_POST);
-
+        $questionId = $_POST['question_id'];
+        $this->checkAnswer($_POST['question_id']);
+        $_SESSION["student"]["answers"][$_POST['question_id']]["score"] = 1;
 
 
         if (intval($_SESSION["student"]["question_counter"]) < $this->maxQuestion && $_POST[time_out_flag]!=1) {
@@ -143,6 +144,35 @@ class exame_one_by_oneController extends BasicController
         $this->view->setTemplate("exame_one_by_one/blank.phtml");
     }
 
+    private function checkAnswer ($qId) {
+        $dbh = connectPDO();
+        $qo = new Questions();
+        $question[0] = $qId;
+
+        // Request for the question and the coresponding options by Id
+        MLog::i("qId :");
+        MLog::iExport ( $qId );
+        $data = $qo->loadFullQuestionsWithOptions($dbh, $question);
+        $sel_question = array_values ($data) [0];
+
+        $q_opt_check = 0;  // 0 : unchecked/right, 1 : wrong
+        MLog::i("student answer :");
+        MLog::iExport ($_SESSION['student']['answers'][$qId]['demo']['question_'.$qId]);
+        MLog::i("sel_question options :");
+        MLog::iExport ( $sel_question ['options'] );
+        foreach ( $sel_question ['options'] as $q_option ) {
+            MLog::i('sel_question q_option id :');
+            MLog::iExport ( $q_option ['id'] );
+//            if (  (  $q_option ['is_right'] == 0
+//                 &&  in_array ( $q_option ['id'], $_SESSION["student"]["answers"][$qId]["demo"]['question_'.$qId] ) )
+//               || (  $q_option ['is_right'] == 1
+//                 && !in_array ( $q_option ['id'], $_SESSION["student"]["answers"][$qId]["demo"]['question_'.$qId] ) ) ) {
+//                MLog::i( 'Answer is Wrong');
+//                $q_opt_check = 1;
+//                break;
+//            }
+        }
+    }
 
 }
 
